896fed6eb53f8aa4b5828605b421aba855848567
Olga <o.panova27@gmail.com>
2022-08-21 00:31:59 +0300

    publish photo to Telegram channel

--- publish_to_bot.py
+++ publish_to_bot.py
@@ -3,3 +3,4 @@ import telegram
 
 bot = telegram.Bot(token='5757191574:AAFWlVI8zFimFbwb-hr07TKEm0ZW7kKaizg')
 bot.send_message(text='Hello World!', chat_id="@epic_space")
+bot.send_photo(chat_id="@epic_space", photo=open('images/nasa_apod_0.jpg', 'rb'))
